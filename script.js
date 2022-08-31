3e5c54c9d382914fe4883d6da01225c7e123d566
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2022-09-01 01:54:51 +0900

    Create upload button.

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-// ページの読み込みを待つ
-
-
-function keypress(mykey,mykeycode){ //キー入力イベント
+var myCanvas=[];
+var ctx=[];
+    
+/*function keypress(mykey,mykeycode){ //キー入力イベント
     if(mykey=="z"){
     }
 }
-
-window.addEventListener('load', init); //ロードイベント登録
-window.addEventListener('DOMContentLoaded', function(){ ///キー入力イベント登録
-    window.addEventListener("keydown", function(e){
+*/
+window.addEventListener('DOMContentLoaded', function(){
+    for(var i = 0;i < 4;i++) {
+        myCanvas[i]= this.document.getElementById("canvasStep" + (i+1));
+        ctx[i]=myCanvas[i].getContext("2d");
+    }/*
+    window.addEventListener("keydown", function(e){ // key input event
       keypress(e.key,e.keyCode);
-    });
+    });*/
 });
-
-function init() {
-    //ローディング処理////////////////////////////////////////
-
-    // サイズを指定
-    const width = 960;
-    const height = 540;
-
-    //2Dの処理
-    ctx2d=document.getElementById("myCanvas2").getContext("2d");
-    ctx2d.width = width;
-    ctx2d.height = height;
-
-    tick();
-
-    function tick() {
-        t++;//システム系の処理
-        //2次元のリセット処理
-        ctx2d.clearRect(0,0,width,height);
-
-        requestAnimationFrame(tick);
+function switchCanvas(canvasNum){ // switch a visible canvas 
+    for(var i = 0;i < 4;i++){
+        if(i==canvasNum){
+            myCanvas[i].style.display = "absolute";
+        } else {
+            myCanvas[i].style.display = "none";
+        }
     }
+}
+function uploadFile(files){ //ここの解読から
+    switchCanvas(0);
+    var reader = new FileReader();              // エ　ローカルファイルの処理
+    reader.onload = function(event) {           // オ　ローカルファイルを読込後処理
+        var img = new Image();                 // カ　　画像ファイルの処理
+        img.onload = function() {              // キ　　　画像ファイル読込後の処理
+            ctx[0].drawImage(img, 0, 0);          // コ　　　　　画像をcanvasに表示
+        }                                       // サ　
+        img.src = event.target.result;         // シ　　　画像を読み込む　
+    }                                           // ス　
+    reader.readAsDataURL(files[0]);             // セ　ローカルファイルを読み込む　　
+}
+
+function generateMaster(){
+
 }
