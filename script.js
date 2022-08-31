611a011704c27a25363eda5def6ca0da0c998823
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2022-08-31 19:31:20 +0900

    Initial commit.

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-var t = 0; //time
-var mode = 0; //mode 0タイトル画面　1セレクト画面　2プレイ画面　3リザルト画面　
-var animationmax = 1; //nextmodeにうつるタイミング
-var animationcnt=0; //アニメーションのカウンター 1でスタート -1処理前
-var nextmode=0; //次にうつるモード
-
 // ページの読み込みを待つ
 
 
 function keypress(mykey,mykeycode){ //キー入力イベント
     if(mykey=="z"){
-        window.alert("z");
     }
 }
 
@@ -38,9 +31,6 @@ function init() {
         t++;//システム系の処理
         //2次元のリセット処理
         ctx2d.clearRect(0,0,width,height);
-
-        ctx2d.fillStyle="rgba(255,255,0,1.0)";
-        ctx2d.fillRect(30+t,30,30,30);
 
         requestAnimationFrame(tick);
     }
