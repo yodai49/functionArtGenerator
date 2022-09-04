653b57ce2628367af5b5c7fa055e26803cf56c1a
82998022+yodai49@users.noreply.github.com <cbasicjavaasembler@yahoo.co.jp>
2022-09-05 00:42:57 +0900

    Upload line detecting system.

--- Function3.js
+++ Function3.js
@@ -1,7 +1,9 @@
 var lineVote=[];
 var voteDiv=200;
 var normalizeParam=[];
-var lines=[]; //直線　形式は{alpha:--, beta:--} xsinα-ycosα+β=0
+var Objects={ // 描画する図形の一覧
+        lines:[], // 直線　形式は{alpha:--, beta:--} xsinα-ycosα+β=0
+    };
 
 function generateFunctionMaster(){
     detectLine();
@@ -32,15 +34,16 @@ function normalizeData(keyName){ //データの標準化を行う関数
     normalizeParam[keyName].max=max;
     normalizeParam[keyName].min=min;
 }
+
 function drawLine(){ //linesにある直線を描画する
     var drawPoints=[];
     var x_mY,x_MY,y_mX,y_MX; // x_mYは、yが最小の時のxの値
-    for(var i =0;i < lines.length;i++){
+    for(var i =0;i < Objects.lines.length;i++){
         drawPoints=[];
-        x_mY=(Math.cos(lines[i].alpha)*minY-lines[i].beta)/Math.sin(lines[i].alpha);
-        x_MY=(Math.cos(lines[i].alpha)*maxY-lines[i].beta)/Math.sin(lines[i].alpha);
-        y_mX=(Math.sin(lines[i].alpha)*minX+lines[i].beta)/Math.cos(lines[i].alpha);
-        y_MX=(Math.sin(lines[i].alpha)*maxX+lines[i].beta)/Math.cos(lines[i].alpha);
+        x_mY=(Math.cos(Objects.lines[i].alpha)*minY-Objects.lines[i].beta)/Math.sin(Objects.lines[i].alpha);
+        x_MY=(Math.cos(Objects.lines[i].alpha)*maxY-Objects.lines[i].beta)/Math.sin(Objects.lines[i].alpha);
+        y_mX=(Math.sin(Objects.lines[i].alpha)*minX+Objects.lines[i].beta)/Math.cos(Objects.lines[i].alpha);
+        y_MX=(Math.sin(Objects.lines[i].alpha)*maxX+Objects.lines[i].beta)/Math.cos(Objects.lines[i].alpha);
         if(minX<x_mY && x_mY < maxX){
             drawPoints.push({x:x_mY,y:minY});
         }
@@ -68,11 +71,61 @@ function drawLine(){ //linesにある直線を描画する
             ctx[2].stroke();
         } else {
             console.log("Failed drawing line. "); // ERROR
-            console.log(lines[i],drawPoints,x_mY,x_MY,y_mX,y_MX);
+            console.log(Objects.lines[i],drawPoints,x_mY,x_MY,y_mX,y_MX);
         }
     
     }
 
+} 
+function searchCluster(clusterList,div,threshold,x,y){ // x,yの位置を探索して、クラスターに属するリストを返却する
+    if(lineVote[x][y]>=threshold){
+        clusterList.push({x:x,y:y});
+        lineVote[x][y]=0;
+        clusterList.concat(searchCluster([],div,threshold,(x+1)%div,y));
+        clusterList.concat(searchCluster([],div,threshold,(x-1+div)%div,y));
+        clusterList.concat(searchCluster([],div,threshold,x,(y+1)%div));
+        clusterList.concat(searchCluster([],div,threshold,x,(y-1+div)%div));
+        return clusterList;
+    } else {
+        lineVote[x][y]=0;
+        return [];
+    }
+}
+
+function clusteringLine(div,threshold){ // 連結成分ごとにクラスタリング　threshold以下は全て0、採用されたもののみ1にする
+    var tempList=[];
+    var aveX,aveY;
+    var flg=0, emptyLine=0;
+    for(var i = 0;i < div;i++){ // 1つもthresholdを超える点がない位置を探索　(連結性が維持されるように)
+        flg=1;
+        for(var j = 0;j < div;j++){
+            if(lineVote[j][i]>threshold){
+                flg=0;
+                break;
+            }
+        }
+        if(flg==1) {
+            emptyLine=i;
+            break;
+        }
+    }
+    for(var i = 0;i < div;i++){
+        for(var j = 0;j < div;j++){
+            tempList=searchCluster([],div,threshold,i,j);
+            if(tempList.length!=0){
+                aveX=0,aveY=0;
+                for(var k = 0;k < tempList.length;k++){
+                    aveX+=tempList[k].x;
+                    if(tempList[k].x<emptyLine) aveX+=voteDiv;
+                    aveY+=tempList[k].y;
+                }
+                aveX/=tempList.length;
+                aveY/=tempList.length;
+                aveX=(aveX+voteDiv)%voteDiv;
+                lineVote[Math.round(aveX)][Math.round(aveY)]=1;
+            }
+        }
+    }
 }
 
 function detectLine(){ // 直線を検出する
@@ -94,42 +147,36 @@ function detectLine(){ // 直線を検出する
     for(var i = 0;i < vertex.length;i++){ // 投票
         votePos1=Number(Math.round((vertex[i].voteAlpha-normalizeParam.voteAlpha.min)/(normalizeParam.voteAlpha.max-normalizeParam.voteAlpha.min)*(voteDiv-1)));
         votePos2=Number(Math.round((vertex[i].voteBeta-normalizeParam.voteBeta.min)/(normalizeParam.voteBeta.max-normalizeParam.voteBeta.min)*(voteDiv-1)))
-        lineVote[votePos1][votePos2]+=4;
-        lineVote[(votePos1+1+voteDiv)%voteDiv][votePos2]+=2;
-        lineVote[(votePos1-1+voteDiv)%voteDiv][votePos2]+=2;
-        lineVote[votePos1][(votePos2+1+voteDiv)%voteDiv]+=2;
-        lineVote[votePos1][(votePos2-1+voteDiv)%voteDiv]+=2;
-        lineVote[(votePos1+1+voteDiv)%voteDiv][(votePos2+1+voteDiv)%voteDiv]+=1;
-        lineVote[(votePos1-1+voteDiv)%voteDiv][(votePos2-1+voteDiv)%voteDiv]+=1;
-        lineVote[(votePos1+1+voteDiv)%voteDiv][(votePos2+1+voteDiv)%voteDiv]+=1;
-        lineVote[(votePos1-1+voteDiv)%voteDiv][(votePos2-1+voteDiv)%voteDiv]+=1;
+        if(vertex[i].power>1){
+            lineVote[votePos1][votePos2]+=4*vertex[i].power;
+            lineVote[(votePos1+1+voteDiv)%voteDiv][votePos2]+=2*vertex[i].power;
+            lineVote[(votePos1-1+voteDiv)%voteDiv][votePos2]+=2*vertex[i].power;
+            lineVote[votePos1][(votePos2+1+voteDiv)%voteDiv]+=2*vertex[i].power;
+            lineVote[votePos1][(votePos2-1+voteDiv)%voteDiv]+=2*vertex[i].power;
+            lineVote[(votePos1+1+voteDiv)%voteDiv][(votePos2+1+voteDiv)%voteDiv]+=1*vertex[i].power;
+            lineVote[(votePos1-1+voteDiv)%voteDiv][(votePos2-1+voteDiv)%voteDiv]+=1*vertex[i].power;
+            lineVote[(votePos1+1+voteDiv)%voteDiv][(votePos2+1+voteDiv)%voteDiv]+=1*vertex[i].power;
+            lineVote[(votePos1-1+voteDiv)%voteDiv][(votePos2-1+voteDiv)%voteDiv]+=1*vertex[i].power;
+        }
     }
+    
+    clusteringLine(voteDiv,voteDiv*voteDiv*lineDetectThreshold); // 連結成分ごとにクラスタリングする 閾値以下の値は削除
+
     var alpha,beta,nAlpha,nBeta;
+    
     for(var i = 0;i < voteDiv;i++){
         for(var j = 0;j < voteDiv;j++){
             ctx[3].fillStyle="rgba(255,0,0," + lineVote[i][j]/12 + ")";
             ctx[3].fillRect(i/voteDiv*myCanvas[3].width,j/voteDiv*myCanvas[3].height,myCanvas[3].width/voteDiv,myCanvas[3].height/voteDiv);
-            if(lineVote[i][j]>voteDiv*voteDiv*lineDetectThreshold){ //直線検出
+            if(lineVote[i][j]==1){ //直線検出
                 nAlpha=i/(voteDiv-1)*(normalizeParam.voteAlpha.max-normalizeParam.voteAlpha.min)+normalizeParam.voteAlpha.min;
                 nBeta=j/(voteDiv-1)*(normalizeParam.voteBeta.max-normalizeParam.voteBeta.min)+normalizeParam.voteBeta.min;
                 alpha=normalizeParam.voteAlpha.mean+normalizeParam.voteAlpha.s*nAlpha;
                 beta=normalizeParam.voteBeta.mean+normalizeParam.voteBeta.s*nBeta; //標準化前に戻す
-                lines.push({alpha:alpha,beta:beta});
-                console.log(nAlpha + ", " + nBeta + "," +alpha + "," + beta);
+                Objects.lines.push({alpha:alpha,beta:beta});
                 ctx[3].fillStyle="rgba(0,255,0,1)";
                 ctx[3].fillRect(i/voteDiv*myCanvas[3].width,j/voteDiv*myCanvas[3].height,myCanvas[3].width/voteDiv,myCanvas[3].height/voteDiv);
             }
         }
     }
-    
-    /*
-    ctx[3].fillStyle="rgba(255,0,0,0.008)";
-    for(var i = 0;i < vertex.length;i++){
-        ctx[3].moveTo((vertex[i].voteAlpha/4+0.5)*myCanvas[3].width,
-                      (vertex[i].voteBeta/5+0.5)*myCanvas[3].height);
-        ctx[3].arc((vertex[i].voteAlpha/4+0.5)*myCanvas[3].width,
-                   (vertex[i].voteBeta/5+0.5)*myCanvas[3].height,
-                   3,0,Math.PI*2);
-        ctx[3].fill();
-    }*/
 }
--- Vertex2.js
+++ Vertex2.js
@@ -4,7 +4,7 @@ var edgeImgData;
 function generateVertexMaster(){ // myCanvas[1]の画像をベクタ化してmyCanvas[2]にセット
     imgData=ctx[1].getImageData(0,0,myCanvas[1].width,myCanvas[1].height);    
     data=imgData.data;
-    ctx[2].drawImage(myCanvas[1],0,0,myCanvas[1].width,myCanvas[1].height);
+//    ctx[2].drawImage(myCanvas[1],0,0,myCanvas[1].width,myCanvas[1].height);
     generateVertex();
     edgeImgData=ctx[1].getImageData(0,0,myCanvas[0].width,myCanvas[0].height).data;
     calcGradient();
@@ -17,15 +17,17 @@ function drawGradient(){
     ctx[2].lineWidth=1;
     ctx[2].strokeStyle="rgba(0,255,0,1)";
     for(var i = 0; i<vertex.length;i++){
-        theta=vertex[i].atan;
-        ctx[2].moveTo(
-            vertex[i].x - maxRad * Math.cos(theta),
-            vertex[i].y + maxRad * Math.sin(theta)
-        );
-        ctx[2].lineTo(
-            vertex[i].x + maxRad * Math.cos(theta),
-            vertex[i].y - maxRad * Math.sin(theta),
-        );
+        if(vertex[i].power>1){
+            theta=vertex[i].atan;
+            ctx[2].moveTo(
+                vertex[i].x - maxRad * Math.cos(theta),
+                vertex[i].y + maxRad * Math.sin(theta)
+            );
+            ctx[2].lineTo(
+                vertex[i].x + maxRad * Math.cos(theta),
+                vertex[i].y - maxRad * Math.sin(theta),
+            );
+        }
     }
     ctx[2].stroke();
 }
@@ -35,7 +37,7 @@ function calcGradient(){ //それぞれの頂点の傾きを計算する
     var xPos,yPos,dataPos;
     var aveX1,aveY1,aveX2,aveY2;
     var maxTheta,maxThetaVal=-1;
-    var tanVote=0;
+    var tanVote=0, avePerTheta=0;
     for(var i = 0;i < arcNum;i++){ //角度をあらかじめ計算しておく
         arc[i]={sin:Math.sin(i/arcNum*Math.PI*2),
                 cos:Math.cos(i/arcNum*Math.PI*2)}
@@ -43,6 +45,7 @@ function calcGradient(){ //それぞれの頂点の傾きを計算する
     for(var i = 0;i < vertex.length;i++){
         maxThetaVal=-1;
         maxTheta=-1;
+        avePerTheta=0;
         for(var j = 0;j < arcNum/2;j++){
             tanVote=0;
             for(var k = -radDiv;k <= radDiv;k++){
@@ -59,9 +62,12 @@ function calcGradient(){ //それぞれの頂点の傾きを計算する
                 maxThetaVal=tanVote;
                 maxTheta=(j/arcNum)*Math.PI*2;
             }
+            avePerTheta+=tanVote;
         }
+        avePerTheta/=(arcNum/2);
         vertex[i].tan=Math.tan(maxTheta);
         vertex[i].atan=maxTheta;
+        vertex[i].power=(maxThetaVal-avePerTheta)/(255*radDiv);
     }
 }
 
@@ -88,11 +94,11 @@ function generateVertex(){ // ベクタ化する
                 lastY=i;
             }
         }
-    }
+    }/*
     ctx[2].fillStyle="rgba(255,0,0,0.8)";
     for(var i = 0;i < vertex.length;i++){       
         ctx[2].moveTo(vertex[i].x,vertex[i].y); 
         ctx[2].arc(vertex[i].x,vertex[i].y,2,0,Math.PI*2);
     }
-    ctx[2].fill();
+    ctx[2].fill();*/
 }
